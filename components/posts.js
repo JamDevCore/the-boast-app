1539a440cf214db9ed125aa773edb43a27dfae6f
James Harding <jamesharding@Jamess-MBP.broadband>
2021-06-16 10:30:50 +0100

    Pushing new document

--- components/posts.js
+++ components/posts.js
@@ -173,7 +173,30 @@ const Posts = ({
         </div>
 
       </div>
-    </div>) : null}
+    </div>) : (<div className="w-2/3 m-4 bg-white shadow overflow-hidden sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">Create a new post to get started</h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">Start communicating with your web users</p>
+      </div>
+      <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+        <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+          <div className="sm:col-span-2">
+            <dt className="my-4 text-sm font-medium text-gray-500">Communicating with your web visitors can increase engagement and conversion, whilst reducing churn and time spent on support</dt>
+            <dt className="my-4 text-sm font-medium text-gray-500">Try sharing news, your latest features, or getting your visitors opinion on what your planning</dt>
+            <dd className="mt-4 text-sm text-gray-900">
+            <button
+        type="button"
+        onClick={() => {
+          router.push('/new-post');
+        }}
+        className="inline-flex items-center px-4 py-2 mt-4 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Create a post
+      </button>
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>)}
   </div>);
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,53 +1,64 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { signIn, signOut, getSession, jwt } from 'next-auth/client'
+
+
 const sheet = new ServerStyleSheet();
 class MyDocument extends Document {
-static async getInitialProps(ctx) {
-const sheet = new ServerStyleSheet();
-const originalRenderPage = ctx.renderPage;
-try {
-ctx.renderPage = () =>
-originalRenderPage({
-// useful for wrapping the whole react tree
-enhanceApp: (App) => (props) =>
-sheet.collectStyles(
-<>
-<App {...props} />
-</>,
-),
-// useful for wrapping in a per-page basis
-enhanceComponent: (Component) => Component,
-});
-// Run the parent `getInitialProps`, it now includes the custom `renderPage`
-const initialProps = await Document.getInitialProps(ctx);
-return {
-        ...initialProps,
-        styles: (
-        <>
-        {initialProps.styles}
-        {sheet.getStyleElement()}
-        </>
-        ),
-    };
-} finally {
-sheet.seal();
-}
-}
-render() {
-return (
-<Html lang="en">
-<Head>
-<script>{console.log('heello')}</script>
-<script async data-userId="60a2747d4652a216c309d08e" data-defaultButton="false" data-buttonText="What's new" src="https://unpkg.com/boast-init@latest/umd/boast-init.min.js"></script>
-</Head>
-<body>
-<Main>
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+        const session = await getSession(ctx);
+        const path = ctx.pathname
+        MyDocument.getPath = path;
+        MyDocument.getSession = await getSession(ctx);
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    // useful for wrapping the whole react tree
+                    enhanceApp: (App) => (props) => {
+                        this.props = props;
+                        return sheet.collectStyles(
+                            <>
+                
+                <App {...props} />
+            </>
+                        , )
+                    },
+                    // useful for wrapping in a per-page basis
+                    enhanceComponent: (Component) => Component
+                , });
+            // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps
+                , styles: (
+                    <>
+                    {initialProps.styles}
+                    {sheet.getStyleElement()}
+                    </>
+                )
+            , };
+        } finally {
+            sheet.seal();
+        }
+    }
+    render(props) {
+        console.log('myprops', MyDocument.getSession)
+        return (
+            <Html lang="en">
+                <Head>
+                <script>{console.log('heello')}</script>
+                {MyDocument.getSession.user.id && MyDocument.getPath !== '/feed/[id]' && <script async data-userId={MyDocument.getSession.user.id} data-defaultButton="false" data-buttonText="What's new" src="https://unpkg.com/boast-init@latest/umd/boast-init.min.js"></script>}
+                </Head>
+                <body>
+                <Main>
 
-</Main>
-<NextScript />
-</body>
-</Html>
-);
-}
+                </Main>
+                <NextScript />
+                </body>
+                </Html>
+        );
+    }
 }
 export default MyDocument;
