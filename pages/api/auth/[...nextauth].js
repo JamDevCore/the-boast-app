e3751c2d5e8237b39031f3a90daa3557cdb9d1c1
James Harding <jamesharding@Jamess-MBP.broadband>
2021-06-17 14:20:54 +0100

    Updated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,22 +2,16 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorizationUrl: process.env.NEXTAUTH_URL
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
     Providers.Email({
         server: process.env.EMAIL_SERVER,
         from: process.env.EMAIL_FROM
-        // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
       })
-    // ...add more providers here
   ],
-
-  // A database is optional, but required to persist accounts in a database
   database: process.env.MONGODBURL, 
   session: {
     jwt: true,
