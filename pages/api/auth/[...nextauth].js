b3b61aaaad9be1d9aa919d4951d2a897539aa096
James Harding <jamesharding@Jamess-MBP.broadband>
2021-06-17 14:11:58 +0100

    Updatd json

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,7 +13,7 @@ export default NextAuth({
         server: process.env.EMAIL_SERVER,
         from: process.env.EMAIL_FROM
         // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
-      }),
+      })
     // ...add more providers here
   ],
 
