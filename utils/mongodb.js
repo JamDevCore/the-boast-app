005934a46e17a5f276d6d628e91524a530e635e9
James Harding <jamesharding@Jamess-MacBook-Pro.local>
2021-06-17 12:06:39 +0100

    Updated mongodburl

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb'
 const ObjectId = require('mongodb').ObjectId;
-const { mongodburl } = process.env
+const { MONGODBURL } = process.env
 
 const MONGODB_DB = 'Boast-Core';
 
