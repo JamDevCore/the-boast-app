58f43d7ad7ed9b33fc97fb0b3492dd7f4a8b8352
James Harding <jamesharding@Jamess-MBP.broadband>
2021-06-30 12:12:22 +0100

    Updated views api

--- pages/api/views.js
+++ pages/api/views.js
@@ -5,11 +5,19 @@ import { ObjectId } from "bson";
 export default async function handler(req, res) {
     const { body } = req;
     const { db  }= await connectToDatabase();
+    try {
     console.log('yolo')
     console.log(req.body.userId);
     if(req.method ==='POST') {
-        await db.collection('users').findOne({ userId: ObjectId(req.body.userId )});
-        return account;
+        await db.collection('users').updateOne({ userId: ObjectId(req.body.userId )}, {
+            $inc: {
+                viewsThisPeriod: 1,
+            }
+        });
+
       }
-     
+     res.send(200)
+    } catch (err) {
+      res.send(400)
+    }
   }
