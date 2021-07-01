4b5ba475529e5a20c6e0ff8c38f09a3dab1bfdff
James Harding <jamesharding@Jamess-MBP.broadband>
2021-07-01 11:30:52 +0100

    Updted new branch

--- pages/index.js
+++ pages/index.js
@@ -352,6 +352,8 @@ export default function Page() {
           onClick={() => signIn()}
           className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 sm:w-auto"
         >
+
+          
           Sign up
         </button>
       </div>
